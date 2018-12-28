2b9ca0546621adf744149a2fd47cb3dcb51ab508
Paul Sherwood <paul.sherwood@codethink.co.uk>
2018-12-28 10:31:48 +0100

    Tidy up docs and Makefile

--- cycle.sh
+++ cycle.sh
@@ -13,10 +13,5 @@ rm -fr /home/builder/mod-workdir/plugins/alo.lv2
 cp -r /tmp/moddevices/alo /home/builder/mod-plugin-builder/plugins/package
 ./build alo
 
-# copy gui
-cp -r /tmp/moddevices/alo/source/alo.lv2/modgui /home/builder/mod-workdir/plugins/alo.lv2
-rm -fr /tmp/moddevices/lv2-test-area/*
-cp -r /home/builder/mod-workdir/plugins/alo.lv2 /tmp/moddevices/lv2-test-area
-
 # deploy
 cd /home/builder/mod-workdir/plugins && tar cz alo.lv2 | base64 | curl -F 'package=@-' http://192.168.51.1/sdk/install
